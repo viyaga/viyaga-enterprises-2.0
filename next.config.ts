a564f4c7a1b9da06e2f774bebde0e4362b99e652
viyaga <mohan2609kumar@gmail.com>
2025-04-21 14:19:36 +0530

    output: "export",

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
 };
 
 export default withPayload(nextConfig);
--- src/app/(app)/(viyaga)/products/[slug]/page.tsx
+++ src/app/(app)/(viyaga)/products/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import ProductDetailsPage from "@/components/pages/product-details";
 
-const page = () => {
+const page = async ({ params }: { params: { slug: string } }) => {
+  const { slug } = await params;
+  console.log({slug});
+  
   return <ProductDetailsPage />;
 };
 
