24d43bea72899d6e32a85ad292c74801e4c49cbc
Viyaga Enterprises <114681544+viyaga@users.noreply.github.com>
2025-04-21 22:52:53 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default withPayload(nextConfig);
