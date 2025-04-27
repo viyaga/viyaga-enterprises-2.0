312fe2cbcbd4dc841b9b2fc4f0f17f9b18298c6c
viyaga <mohan2609kumar@gmail.com>
2025-04-27 13:36:02 +0530

    api

--- src/app/(payload)/api/revalidate/route.ts
+++ src/app/(payload)/api/revalidate/route.ts
@@ -1,30 +1,45 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { revalidatePath, revalidateTag } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
 
-const revalidate = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { tag, path, token } = req.query;
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const token = searchParams.get('token');
+  const path = searchParams.get('path');
+  const tag  = searchParams.get('tag');
 
-    // Check if the token is valid
-    if (token !== process.env.PAYLOAD_SECRET) {
-        return res.status(403).json({ message: 'Invalid token' });
+  if (token !== process.env.PAYLOAD_SECRET) {
+    return NextResponse.json(
+      { message: 'Invalid token' },
+      { status: 403 }
+    );
+  }
+
+  try {
+    if (tag) {
+      // Revalidate by tag
+      revalidateTag(tag);
+      return NextResponse.json(
+        { message: `Revalidated by tag: ${tag}` },
+        { status: 200 }
+      );
+    } else if (path) {
+      // Revalidate by path
+      revalidatePath(path);
+      return NextResponse.json(
+        { message: `Revalidated by path: ${path}` },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: 'Missing either tag or path for revalidation' },
+        { status: 400 }
+      );
     }
-
-    try {
-        if (tag) {
-            revalidateTag(tag as string);
-            return res.status(200).json({ message: `Revalidated by tag: ${tag}` });
-
-        } else if (path) {
-            revalidatePath(path as string);
-            return res.status(200).json({ message: `Revalidated by path: ${path}` });
-
-        } else {
-            return res.status(400).json({ message: 'Missing either tag or path for revalidation' });
-        }
-    } catch (error) {
-        console.error('Error during revalidation:', error);
-        return res.status(500).json({ message: 'Error during revalidation' });
-    }
-};
-
-export default revalidate;
+  } catch (error) {
+    console.error('Error during revalidation:', error);
+    return NextResponse.json(
+      { message: 'Error during revalidation' },
+      { status: 500 }
+    );
+  }
+}
