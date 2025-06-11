936190b78fec2e387bd9687c20f557a4b658fac3
viyaga <mohan2609kumar@gmail.com>
2025-06-12 02:20:31 +0530

    push

--- src/components/pages/product-details/index.tsx
+++ src/components/pages/product-details/index.tsx
@@ -12,15 +12,14 @@ const ProductDetailsPage = ({ docs }: { docs: Product[] }) => {
 
   return (
     <section className="bg-gradient-to-b from-[#f0f9ff] to-[#f0f9ff] dark:from-[#0f172a] dark:to-[#0f172a]">
-      <div
-        className="bg-gradient-to-b from-[#f4cc77] via-[#f7d895] to-[#fff] 
+      <div className="pt-36 space-y-12 flex flex-col md:flex-row gap-8 max-w-6xl mx-auto p-6">
+        <div
+          className="bg-gradient-to-b from-[#f4cc77] via-[#f7d895] to-[#fff] 
       dark:from-[#00182e] dark:via-[#00334d] dark:to-[#00182e] "
-      >
-        <div className="pt-36 space-y-12 flex flex-col md:flex-row gap-8 max-w-6xl mx-auto p-6">
+        >
           <ProductThumbnail product={product} />
-          
-          <ProductInfo product={product} />
         </div>
+        <ProductInfo product={product} />
       </div>
       {product.isSubscription && product.subscriptionPlans?.length > 0 && (
         <SubscriptionPlans plans={product.subscriptionPlans} />
