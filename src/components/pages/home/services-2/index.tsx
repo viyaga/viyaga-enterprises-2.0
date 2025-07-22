4bed28f7b5a586a627251b2f9488876f767dc240
viyaga <mohan2609kumar@gmail.com>
2025-07-22 13:34:54 +0530

    images

--- src/components/pages/home/services-2/index.tsx
+++ src/components/pages/home/services-2/index.tsx
@@ -8,23 +8,27 @@ import Image from "next/image";
 const services = [
   {
     id: 1,
-    img: "/images/ai-services.png",
+    img: "/images/services/ai-services.png",
     title: "AI Agents & Automation",
     counter: 46,
+    bgColor: "bg-red-500",
   },
   {
     id: 2,
-    img: "/images/web-services.png",
+    img: "/images/services/web-services.png",
     title: "Full-Stack Web Applications",
     counter: 35,
+    bgColor: "bg-teal-600",
   },
   {
     id: 3,
-    img: "/images/mobile-app-services.png",
+    img: "/images/services/mobile-app-services.png",
     title: "Scalable Mobile Solutions",
     counter: 23,
+    bgColor: "bg-yellow-600",
   },
 ];
+
 
 const ServicesSection = () => {
   const ref = useRef(null);
@@ -80,29 +84,18 @@ const ServicesSection = () => {
                 variants={listVariants}
                 className={`
                   flex items-center gap-4 p-5 rounded-2xl transition-all duration-300 cursor-pointer
-                  ${
-                    activeServiceId === service.id
-                      ? "bg-gray-200 dark:bg-white/20"
-                      : "bg-gray-100 hover:bg-gray-200 dark:bg-white/10 dark:hover:bg-white/20"
+                  ${activeServiceId === service.id
+                    ? "bg-gray-200 dark:bg-white/20"
+                    : "bg-gray-100 hover:bg-gray-200 dark:bg-white/10 dark:hover:bg-white/20"
                   }
                 `}
               >
-                <div
-                  className={`
-                    w-12 h-12 rounded-full flex items-center justify-center
-                    ${service.id === 1
-                      ? "bg-red-500"
-                      : service.id === 2
-                      ? "bg-teal-600"
-                      : "bg-yellow-600"}
-                  `}
-                >
+                <div className={`relative w-12 h-12 rounded-full overflow-hidden ${service.bgColor}`}>
                   <Image
                     src={service.img}
-                    width={24}
-                    height={24}
                     alt={service.title}
-                    className="p-1 sm:p-2 md:p-3"
+                    fill
+                    className="object-cover"
                   />
                 </div>
                 <div className="text-left">
