432a9c6833e3a8471b3b6c4e00f47c57d25ddaeb
viyaga <mohan2609kumar@gmail.com>
2025-08-06 11:05:02 +0530

    Suspense

--- src/app/(app)/(viyaga)/layout.tsx
+++ src/app/(app)/(viyaga)/layout.tsx
@@ -1,14 +1,16 @@
 import ActivityTracker from "@/components/activity-tracker";
 import ViyagaLayout from "@/components/layouts/viyaga";
 import WebForgeAI from "@/components/web-forge-aI";
-import React from "react";
+import React, { Suspense } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ViyagaLayout>
       {children}
-      <ActivityTracker />
-      <WebForgeAI />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ActivityTracker />
+        <WebForgeAI />
+      </Suspense>
     </ViyagaLayout>
   );
 };
